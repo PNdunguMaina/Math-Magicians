be1fbc203ec154b42a8760b57260549e0d177779
PNdunguMaina <ndunguwamaina@gmail.com>
2022-08-18 16:38:02 +0300

    Add events functionality to the calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,107 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
-import Digits from './Digits';
+
+import React, { Component, Fragment } from 'react';
+import calculate from '../logic/calculate';
 
 // implement calculator component
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { total: '', next: '', operation: '' };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // event handler method
+  handleClick = (event) => {
+    const button = event.target.id;
+    this.setState((state) => calculate({ ...state }, button));
+  };
+  // displayed output
+  output = () => {
+    const { total, operation, next } = this.state;
+    return (
+      <React.Fragment>
+        {total} {operation} {next}
+      </React.Fragment>
+    );
+  };
+
   render() {
     return (
       <div className="calculator">
         {/* display */}
         <div className="display">
-          <span>0</span>
+          <span>{this.output()}</span>
         </div>
         {/* main keys */}
         <div className="main-keys">
-          <button>AC</button>
-          <button>+/-</button>
-          <button>%</button>
+          <button onClick={this.handleClick} id="AC">
+            AC
+          </button>
+          <button onClick={this.handleClick} id="+/-">
+            +/-
+          </button>
+          <button onClick={this.handleClick} id="%">
+            %
+          </button>
         </div>
 
         {/* operator keys */}
         <div className="operator-keys">
-          <button>&#247;</button>
-          <button>x</button>
-          <button>-</button>
-          <button>+</button>
-          <button>=</button>
+          <button onClick={this.handleClick} id="&#247;">
+            &#247;
+          </button>
+          <button onClick={this.handleClick} id="x">
+            x
+          </button>
+          <button onClick={this.handleClick} id="-">
+            -
+          </button>
+          <button onClick={this.handleClick} id="+">
+            +
+          </button>
+          <button onClick={this.handleClick} id="=">
+            =
+          </button>
         </div>
 
         {/* digit keys */}
         <div className="digit-keys">
-          <Digits />
+          <button onClick={this.handleClick} id="7">
+            7
+          </button>
+          <button onClick={this.handleClick} id="8">
+            8
+          </button>
+          <button onClick={this.handleClick} id="9">
+            9
+          </button>
+          <button onClick={this.handleClick} id="4">
+            4
+          </button>
+          <button onClick={this.handleClick} id="5">
+            5
+          </button>
+          <button onClick={this.handleClick} id="6">
+            6
+          </button>
+          <button onClick={this.handleClick} id="1">
+            1
+          </button>
+          <button onClick={this.handleClick} id="2">
+            2
+          </button>
+          <button onClick={this.handleClick} id="3">
+            3
+          </button>
           <div className="special-digits">
-            <button>0</button>
-            <button>.</button>
+            <button onClick={this.handleClick} id="0">
+              0
+            </button>
+            <button onClick={this.handleClick} id=".">
+              .
+            </button>
           </div>
         </div>
       </div>
