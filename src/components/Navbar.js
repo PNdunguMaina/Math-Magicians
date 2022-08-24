6a9c0167cbbd602e1c8be27100a9c045e0e9986c
PNdunguMaina <ndunguwamaina@gmail.com>
2022-08-24 19:31:36 +0300

    Style React app via inline styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const navStyle = {
+    color: 'red',
+    top: '0',
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100vw',
+    padding: '3rem',
+    backgroundColor: 'black',
+  };
+
+  const navItemStyle = {
+    display: 'flex',
+    gap: '3rem',
+    fontSize: '2rem',
+    fontWeight: '600',
+    color: 'red',
+  };
+
   return (
-    <div className="navbarContainer">
+    <div className="navbarContainer" style={navStyle}>
       <div className="logo-container">
         <h1 className="nav-logo">Math Magicians</h1>
       </div>
-      <div className="links-container">
-        <Link to="/">Home</Link>
-        <Link to="/calculator">Calculator</Link>
-        <Link to="/quotes">Quote</Link>
+      <div className="links-container" style={navItemStyle}>
+        <Link style={{ color: 'red' }} to="/">
+          Home
+        </Link>
+        <Link style={{ color: 'red' }} to="/calculator">
+          Calculator
+        </Link>
+        <Link style={{ color: 'red' }} to="/quotes">
+          Quote
+        </Link>
       </div>
     </div>
   );
