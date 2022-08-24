e42345f3d960174e6272d4f82fe1ad4bd05de05b
PNdunguMaina <ndunguwamaina@gmail.com>
2022-08-24 18:37:40 +0300

    Create a route for every page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Quotes from './components/Quotes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quotes" element={<Quotes />} />
+          </Routes>
+        </Router>
       </div>
     );
   }
