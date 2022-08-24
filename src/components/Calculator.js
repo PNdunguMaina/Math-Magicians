67c9ced8d82dc541b9dea6eb81a27c22dbd78f2f
PNdunguMaina <ndunguwamaina@gmail.com>
2022-08-24 23:51:49 +0300

    Fix ESLint error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -24,91 +24,89 @@ const Calculator = () => {
     );
   };
 
-  render() {
-    return (
-      <>
-        <header className="calculator-page-heading">
-          <h1>Let&apos;s perform some calculations!</h1>
-        </header>
-        <div className="calculator">
-          {/* display */}
-          <div className="display">
-            <span>{this.output()}</span>
-          </div>
-          {/* main keys */}
-          <div className="main-keys">
-            <button type="submit" onClick={this.handleClick} id="AC">
-              AC
-            </button>
-            <button type="button" onClick={this.handleClick} id="+/-">
-              +/-
-            </button>
-            <button type="button" onClick={this.handleClick} id="%">
-              %
-            </button>
-          </div>
+  return (
+    <>
+      <header className="calculator-page-heading">
+        <h1>Let&apos;s perform some calculations!</h1>
+      </header>
+      <div className="calculator">
+        {/* display */}
+        <div className="display">
+          <span>{result()}</span>
+        </div>
+        {/* main keys */}
+        <div className="main-keys">
+          <button type="submit" onClick={handleClick} id="AC">
+            AC
+          </button>
+          <button type="button" onClick={handleClick} id="+/-">
+            +/-
+          </button>
+          <button type="button" onClick={handleClick} id="%">
+            %
+          </button>
+        </div>
 
-          {/* operator keys */}
-          <div className="operator-keys">
-            <button type="button" onClick={this.handleClick} id="&#247;">
-              &#247;
-            </button>
-            <button type="button" onClick={this.handleClick} id="x">
-              x
-            </button>
-            <button type="button" onClick={this.handleClick} id="-">
-              -
-            </button>
-            <button type="button" onClick={this.handleClick} id="+">
-              +
-            </button>
-            <button type="submit" onClick={this.handleClick} id="=">
-              =
-            </button>
-          </div>
+        {/* operator keys */}
+        <div className="operator-keys">
+          <button type="button" onClick={handleClick} id="&#247;">
+            &#247;
+          </button>
+          <button type="button" onClick={handleClick} id="x">
+            x
+          </button>
+          <button type="button" onClick={handleClick} id="-">
+            -
+          </button>
+          <button type="button" onClick={handleClick} id="+">
+            +
+          </button>
+          <button type="submit" onClick={handleClick} id="=">
+            =
+          </button>
+        </div>
 
-          {/* digit keys */}
-          <div className="digit-keys">
-            <button type="button" onClick={this.handleClick} id="7">
-              7
+        {/* digit keys */}
+        <div className="digit-keys">
+          <button type="button" onClick={handleClick} id="7">
+            7
+          </button>
+          <button type="button" onClick={handleClick} id="8">
+            8
+          </button>
+          <button type="button" onClick={handleClick} id="9">
+            9
+          </button>
+          <button type="button" onClick={handleClick} id="4">
+            4
+          </button>
+          <button type="button" onClick={handleClick} id="5">
+            5
+          </button>
+          <button type="button" onClick={handleClick} id="6">
+            6
+          </button>
+          <button type="button" onClick={handleClick} id="1">
+            1
+          </button>
+          <button type="button" onClick={handleClick} id="2">
+            2
+          </button>
+          <button type="button" onClick={handleClick} id="3">
+            3
+          </button>
+          <div className="special-digits">
+            <button type="button" onClick={handleClick} id="0">
+              0
             </button>
-            <button type="button" onClick={this.handleClick} id="8">
-              8
+            <button type="button" onClick={handleClick} id=".">
+              .
             </button>
-            <button type="button" onClick={this.handleClick} id="9">
-              9
-            </button>
-            <button type="button" onClick={this.handleClick} id="4">
-              4
-            </button>
-            <button type="button" onClick={this.handleClick} id="5">
-              5
-            </button>
-            <button type="button" onClick={this.handleClick} id="6">
-              6
-            </button>
-            <button type="button" onClick={this.handleClick} id="1">
-              1
-            </button>
-            <button type="button" onClick={this.handleClick} id="2">
-              2
-            </button>
-            <button type="button" onClick={this.handleClick} id="3">
-              3
-            </button>
-            <div className="special-digits">
-              <button type="button" onClick={this.handleClick} id="0">
-                0
-              </button>
-              <button type="button" onClick={this.handleClick} id=".">
-                .
-              </button>
-            </div>
           </div>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
 export default Calculator;
