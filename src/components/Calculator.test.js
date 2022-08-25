f30be34a41df3c198b57a7a8ae91d9abf1d8baf7
PNdunguMaina <ndunguwamaina@gmail.com>
2022-08-25 17:42:11 +0300

    Write unit tests with Jest & React Testing Library

---
+++ src/components/Calculator.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom';
+
+import Calculator from './Calculator';
+
+it('performs and displays calculations properly', () => {
+  render(
+    <Router>
+      <Calculator />
+    </Router>
+  );
+  const key1 = screen.getByText('5');
+  const operator = screen.getByText('x');
+  const key2 = screen.getByText('5');
+  const result = screen.getByText('=');
+
+  fireEvent.click(key1);
+  fireEvent.click(operator);
+  fireEvent.click(key2);
+  fireEvent.click(result);
+  const output = screen.getByText('3');
+  expect(output).toBeVisible();
+});
